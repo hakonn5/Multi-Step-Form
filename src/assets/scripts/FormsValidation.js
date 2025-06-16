567edd872dd130912c1132815819b8d0e58b0083
hakonn5 <artiom.zui5@mail.ru>
2025-06-16 18:16:01 +0300

    fix: add missing class for layout

--- src/assets/scripts/FormsValidation.js
+++ src/assets/scripts/FormsValidation.js
@@ -1,1 +1,53 @@
-export default class FormsValidation {}
+export default class FormsValidation {
+    selectors = {
+      stepLabel: "[data-js-form-step]",
+    };
+  
+    stateClasses = {
+      stepLabelActive: "steps__label--active",
+    };
+  
+    stepValidationClasses = {
+      0: Step_0_Validation,
+    };
+  
+    constructor(formStepsControllerInstance) {
+      this.formStepsControllerInstance = formStepsControllerInstance;
+      this.currentStep = this.formStepsControllerInstance.currentStep;
+      this.data = {};
+  
+      this.init();
+    }
+  
+    init = () => {
+      const StepValidator = this.stepValidationClasses[this.currentStep];
+  
+      if (StepValidator) {
+        this.formStepsControllerInstance.renderStep(this.currentStep);
+        new StepValidator(this);
+        this.markCurrentStepLabel();
+      }
+    };
+  
+    saveData = (data = {}) => {
+      this.data = { ...this.data, ...data };
+    };
+  
+    markCurrentStepLabel = () => {
+      this.rootElement = document.querySelector(root);
+      this.stepLabelElements = this.rootElement.querySelectorAll(
+        this.selectors.stepLabel
+      );
+  
+      if (this.stepLabelElements) {
+        this.stepLabelElements.forEach((element, indexElement, labels) => {
+          const isCurrentStep = this.currentStep === indexElement;
+          if (isCurrentStep) {
+            element.classList.toggle(this.stateClasses.stepLabelActive, isCurrentStep);
+          }
+        });
+      }
+    };
+  }
+
+  class Step_0_Validation {}
